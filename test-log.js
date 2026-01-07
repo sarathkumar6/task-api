c20a9e37ca71118eb40d80a8f360ae81ee56e7a5
sarathkumar6 <nagarajsarath@gmail.com>
2026-01-06 22:00:21 -0500

    eslint: fix undef error

--- test-log.js
+++ test-log.js
@@ -1,14 +1,16 @@
-const logger = require('./utils/logger');
+/* eslint-disable no-undef */
+const logger = require("./utils/logger");
 
 console.log("🚀 Starting Splunk Log Test...");
 
-logger.info("Principal Level Test: Audit Log to Splunk", { 
-    action: "MANUAL_TEST", 
-    sarath_status: "Persistence Mode" 
+logger.info("Principal Level Test: Audit Log to Splunk", {
+  action: "MANUAL_TEST",
+  sarath_status: "Persistence Mode",
 });
 
 // Give the network 2 seconds to actually send the data before killing the process
+
 setTimeout(() => {
-    console.log("✅ Wait period over. Checking for Splunk errors above...");
-    process.exit(0);
+  console.log("✅ Wait period over. Checking for Splunk errors above...");
+  process.exit(0);
 }, 2000);
