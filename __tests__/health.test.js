ceaf6c64e6e7e3ebe9e1d9921d096b73d0482a0c
sarathkumar6 <nagarajsarath@gmail.com>
2026-01-13 21:43:52 -0500

    feat: update health test with expected assertions

--- __tests__/health.test.js
+++ __tests__/health.test.js
@@ -5,7 +5,10 @@ describe("Sanity check: Health Endpoint", () => {
   it("should return 200 and a success message", async () => {
     const response = await request(app).get("/health");
     expect(response.statusCode).toEqual(200);
-    expect(response.body).toHaveProperty("status", "UP");
-    expect(response.body).toHaveProperty("message", "Server is healthy");
+    const { status, database, version, dbResponse } = response.body;
+    expect(status).toBe("UP");
+    expect(database).toBe("CONNECTED");
+    expect(version).toBe("2.0-db-check");
+    expect(dbResponse).toBeDefined();
   });
 });
